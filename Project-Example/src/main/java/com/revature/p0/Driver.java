b21a03efcba5473535c4894abf291e6e935ccff2
LiquidPlummer <kyle.plummer@revature.net>
2024-06-27 18:18:08 -0400

    example

---
+++ Project-Example/src/main/java/com/revature/p0/Driver.java
@@ -0,0 +1,17 @@
+package com.revature.p0;
+
+import com.revature.p0.menus.LoginMenu;
+import com.revature.p0.menus.MainMenu;
+import com.revature.p0.menus.RegisterMenu;
+
+public class Driver {
+    public static void main(String[] args) {
+        MenuUtil menuUtil = MenuUtil.getMenuUtil();
+        menuUtil.register(new MainMenu());
+        menuUtil.register(new RegisterMenu());
+        menuUtil.register(new LoginMenu());
+        menuUtil.run();
+
+    }
+
+}
